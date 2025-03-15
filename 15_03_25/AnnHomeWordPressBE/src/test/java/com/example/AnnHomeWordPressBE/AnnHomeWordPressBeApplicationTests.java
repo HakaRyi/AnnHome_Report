11ba65eee24cad51e061b5d45ad014b993ef2d6a
XuanKhang <156918778+HakaRyi@users.noreply.github.com>
2025-03-15 10:19:08 +0700

    Add files via upload

---
+++ 15_03_25/AnnHomeWordPressBE/src/test/java/com/example/AnnHomeWordPressBE/AnnHomeWordPressBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AnnHomeWordPressBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnHomeWordPressBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
